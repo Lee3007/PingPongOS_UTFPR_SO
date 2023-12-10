35a6c027b90fecb839309008f7f23b0dfeac5b3d
Lee3007 <felipe.lee30@gmail.com>
2023-12-10 16:06:41 -0300

    Add: small changes

---
+++ Projeto_A/ping-pong-os---kit-alunos-20230901/ppos_disc.c
@@ -0,0 +1,23 @@
+int disk_mgr_init(int *numBlocks, int *blockSize)
+{
+}
+
+int disk_block_read(int block, void *buffer)
+{
+}
+
+int disk_block_write(int block, void *buffer)
+{
+}
+
+void escalonator_fcfs()
+{
+}
+
+void escalonator_sstf()
+{
+}
+
+void escalonator_cscan()
+{
+}
