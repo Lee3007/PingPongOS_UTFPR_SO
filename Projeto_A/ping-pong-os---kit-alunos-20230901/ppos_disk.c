90256b5349aa339abb868f4e96c72564bef9c13c
Gabriel Freitas Willig <gwillig@pop-os.localdomain>
2023-12-10 15:36:02 -0300

    added write and read

--- Projeto_A/ping-pong-os---kit-alunos-20230901/ppos_disk.c
+++ Projeto_A/ping-pong-os---kit-alunos-20230901/ppos_disk.c
@@ -1,13 +1,38 @@
 #include "ppos_disk.h"
+#include "disk.h"
+#include "ppos-core-globals.h"
+#include "ppos.h"
+
+task_t **disk_queue;
+
+typedef struct disk_task {
+    int val;
+    struct disk_task * next;
+} disk_task_t;
 
 int disk_mgr_init (int *numBlocks, int *blockSize){
-    return 2;
+    return 0;
 }
 
 int disk_block_read (int block, void* buffer){
-    return 1;
+    int read_value = disk_cmd(DISK_CMD_READ, block, buffer); 
+
+    disk_task_t* a = (disk_task_t*) malloc(sizeof(disk_task_t));
+
+    task_suspend(taskExec, disk_queue);
+
+    task_yield();
+
+    return read_value;
 }
 
 int disk_block_write (int block, void* buffer){
-    return 0;
+
+    int write_value = disk_cmd(DISK_CMD_WRITE, block, buffer);
+
+    task_suspend(taskExec, disk_queue);
+
+    task_yield();
+
+    return write_value;
 }
